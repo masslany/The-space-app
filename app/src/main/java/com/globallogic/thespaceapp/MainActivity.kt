9c24ee40ceca79dc86335495f9cd4fa581439a42
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-20 12:06:53 +0200

    Set default theme to dark mode

--- app/src/main/java/com/globallogic/thespaceapp/MainActivity.kt
+++ app/src/main/java/com/globallogic/thespaceapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.globallogic.thespaceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_roadster_details)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
