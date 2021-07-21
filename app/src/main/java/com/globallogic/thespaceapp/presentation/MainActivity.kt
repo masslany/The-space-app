241bad74c7b64e4bb9a05ec1525563b6c1e282e8
Julian Czaja <julian.czaja@globallogic.com>
2021-07-21 14:05:43 +0000

    #8 Created splash screen

--- app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.globallogic.thespaceapp.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.globallogic.thespaceapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Switch splash theme to main theme
+        setTheme(R.style.Theme_TheSpaceApp)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
