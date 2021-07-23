f99a6e72df4922f7310eb6b952609c9f7a32d8d5
Julian Czaja <julian.czaja@globallogic.com>
2021-07-23 08:44:04 +0200

    Created DragonEntity

---
+++ app/src/main/java/com/globallogic/thespaceapp/domain/model/DragonEntity.kt
@@ -0,0 +1,19 @@
+package com.globallogic.thespaceapp.domain.model
+
+import android.net.Uri
+import com.globallogic.thespaceapp.data.remote.response.dragons.Thruster
+
+data class DragonEntity(
+    val name: String,
+    val active: Boolean,
+    val crewCapacity: Int,
+    val description: String,
+    val diameter: Double,
+    val dryMass: Int,
+    val firstFlight: String,
+    val flickrImages: List<String>,
+    val id: String,
+    val wikipedia: Uri,
+    val heightWTrunk: Double,
+    val thrusters: List<Thruster>,
+)
