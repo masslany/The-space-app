df69584efda18c2fbca05d726ba8ae382089a243
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-23 09:13:51 +0200

    WIP: navigate to rocket details

---
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/rockets/RocketDetailsFragment.kt
@@ -0,0 +1,9 @@
+package com.globallogic.thespaceapp.presentation.rockets
+
+import androidx.fragment.app.Fragment
+import com.globallogic.thespaceapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RocketDetailsFragment : Fragment(R.layout.fragment_rocket_details) {
+}
