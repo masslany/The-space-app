57c635b3a61034fbdbad783ae9c19341982c60b7
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-23 11:45:34 +0200

    WIP: Card view items

--- app/src/main/java/com/globallogic/thespaceapp/presentation/rockets/RocketDetailsFragment.kt
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/rockets/RocketDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.globallogic.thespaceapp.presentation.rockets
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.RequestManager
-import com.globallogic.thespaceapp.R
 import com.globallogic.thespaceapp.databinding.FragmentRocketDetailsBinding
 import com.globallogic.thespaceapp.utils.State
 import dagger.hilt.android.AndroidEntryPoint
@@ -52,9 +52,25 @@ class RocketDetailsFragment : Fragment() {
                 State.Loading -> {
                 }
                 is State.Success -> {
-                    glide
-                        .load(state.data.flickrImages.first())
-                        .into(binding.ivRocketDetails)
+                    val rocketInfo = state.data
+                    with(binding) {
+
+                        // Top part
+                        tvRocketDetailsHeadline.text = rocketInfo.name
+                        tvRocketDetailsType.text = rocketInfo.type.uppercase()
+                        Log.d("TAG", "observeUi: ${rocketInfo.description}")
+                        tvRocketDetailsDescription.text = rocketInfo.description
+
+                        glide
+                            .load(rocketInfo.flickrImages.first())
+                            .into(ivRocketDetails)
+
+                        // Rocket size card
+                        cardRocketSize.tvHeight.text = rocketInfo.height.meters.toString()
+                        cardRocketSize.tvDiameter.text = rocketInfo.diameter.meters.toString()
+                        cardRocketSize.tvMass.text = rocketInfo.mass.kg.toString()
+
+                    }
                 }
             }
         }
