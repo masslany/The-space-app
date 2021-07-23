c0e1733ac4cdb4e663da3e1564c87c7a91e1da29
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-23 09:48:05 +0200

    Setup view model

--- app/src/main/java/com/globallogic/thespaceapp/presentation/rockets/RocketDetailsFragment.kt
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/rockets/RocketDetailsFragment.kt
@@ -1,9 +1,62 @@
 package com.globallogic.thespaceapp.presentation.rockets
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.RequestManager
 import com.globallogic.thespaceapp.R
+import com.globallogic.thespaceapp.databinding.FragmentRocketDetailsBinding
+import com.globallogic.thespaceapp.utils.State
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RocketDetailsFragment : Fragment(R.layout.fragment_rocket_details) {
+
+    private var _binding: FragmentRocketDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: RocketDetailsFragmentArgs by navArgs()
+
+    private val viewModel: RocketsSharedViewModel by activityViewModels()
+
+    @Inject
+    lateinit var glide: RequestManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRocketDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeUi()
+
+        viewModel.getRocketByIt(args.rocketId)
+    }
+
+    private fun observeUi() {
+        viewModel.rocket.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is State.Error -> {
+                }
+                State.Loading -> {
+                }
+                is State.Success -> {
+                    glide
+                        .load(state.data.flickrImages.first())
+                        .into(binding.ivRocketDetails)
+                }
+            }
+        }
+    }
 }
