fa46662a8af8e9c4d8ecb71033e6a59f58d35375
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-26 11:36:42 +0200

    Handle up navigation in the toolbar

--- app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
@@ -3,13 +3,19 @@ package com.globallogic.thespaceapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.globallogic.thespaceapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Switch splash theme to main theme
         setTheme(R.style.Theme_TheSpaceApp)
@@ -21,7 +27,15 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 }
