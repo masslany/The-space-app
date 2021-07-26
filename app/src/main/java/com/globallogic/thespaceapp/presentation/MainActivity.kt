c361c7c19c25492790f8855ae6df6edb2dd72932
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-07-26 11:33:29 +0200

    Setup action bar with nav controller

--- app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
+++ app/src/main/java/com/globallogic/thespaceapp/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.globallogic.thespaceapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.globallogic.thespaceapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,5 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 }
